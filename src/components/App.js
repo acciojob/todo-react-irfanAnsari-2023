f9bb83f7f97510658961ceb3a410d17464b5b4a5
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-03-29 10:37:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import Todo from "./Todo";
+import "./../styles/App.css";
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addTask = () => {
+    if (inputValue.trim() !== "") {
+      setTasks([...tasks, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const removeTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter a task"
+      />
+      <button onClick={addTask}>Add Todo</button>
+      <Todo tasks={tasks} removeTask={removeTask} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
